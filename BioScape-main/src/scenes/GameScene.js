a95efdbfbeca099b774841c7e33b4bcd07cb101b
Scorpiojc <jibajajuancarlos84@gmail.com>
2026-05-21 20:33:52 +0200

    Crear MVP jugable con mapa y personaje en Phaser

--- BioScape-main/src/scenes/GameScene.js
+++ BioScape-main/src/scenes/GameScene.js
@@ -1,162 +1,60 @@
-
-import Mutant from "../objects/mutant.js";
-import Drone from "../objects/drone.js";
-import Camera from "../objects/camera.js";
-
 export default class GameScene extends Phaser.Scene {
-
   constructor() {
     super("GameScene");
   }
 
-  create() {
+  preload() {
+    this.load.tilemapTiledJSON("map", "../mapa/mapa.json");
+    this.load.image("terrain", "../mapa/rock_packed.png");
 
-// VARIABLES
-  
-    this.playerHealth = 3;
-    this.playerAmmo = 20;
-    this.keysCollected = 0;
-    this.gameStartTime = this.time.now;
-
-// BACKGROUND
- 
-// PLAYER
-
-//ENEMIES
- 
-
-    this.enemies = this.physics.add.group();
-
-    this.enemyBullets = this.physics.add.group();
-
-    const mutant = new Mutant(
-      this,
-      400,
-      480
-    );
-
-    const drone = new Drone(
-      this,
-      700,
-      150,
-      this.player
-    );
-
-    const camera = new Camera(
-      this,
-      850,
-      200,
-      this.player,
-      this.enemyBullets
-    );
-
-    this.enemies.add(mutant);
-    this.enemies.add(drone);
-    this.enemies.add(camera);
-
-    this.physics.add.collider(
-      this.enemies,
-      this.platforms
-    );
-
-
-    //KEYS
-
-
-   // EXIT DOOR
-  
-
-    this.exitDoor = this.add.rectangle(
-      920,
-      450,
-      40,
-      80,
-      0x00ff00
-    );
-
-    this.physics.add.existing(this.exitDoor, true);
-
-    this.physics.add.overlap(
-      this.player,
-      this.exitDoor,
-      () => {
-
-        if (this.keysCollected >= 3) {
-
-          this.scene.start(
-            "WinScene",
-            {
-              time:
-                Math.floor(
-                  (this.time.now - this.gameStartTime) / 1000
-                ),
-            }
-          );
-        }
-      }
-    );
-
-
-    //PLAYER DAMAGE
-
-
-
-   // PLAYER SHOOTING
-  
-    //CONTROLS
-
-   // HUD
-
+    // Usamos temporalmente el sprite de la compañera disponible en el repo
+    this.load.image("player", "assets/sprites/enemies/mutant.png");
   }
 
+  create() {
+    const map = this.make.tilemap({ key: "map" });
+    const tileset = map.addTilesetImage("rock_packed", "terrain");
+    const platforms = map.createLayer("plataformas", tileset, 0, 0);
+
+    platforms.setCollisionByExclusion([0]);
+
+    this.player = this.physics.add.sprite(64, 64, "player");
+    this.player.setCollideWorldBounds(true);
+    this.player.setBounce(0.02);
+    this.player.setSize(14, 16);
+
+    this.physics.add.collider(this.player, platforms);
+
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.startFollow(this.player, true, 0.08, 0.08);
+    this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.keys = this.input.keyboard.addKeys({
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+      jump: Phaser.Input.Keyboard.KeyCodes.W,
+    });
+  }
 
   update() {
+    const movingLeft = this.cursors.left.isDown || this.keys.left.isDown;
+    const movingRight = this.cursors.right.isDown || this.keys.right.isDown;
 
-    
-    //PLAYER MOVEMENT
-   
+    if (movingLeft) {
+      this.player.setVelocityX(-170);
+      this.player.setFlipX(true);
+    } else if (movingRight) {
+      this.player.setVelocityX(170);
+      this.player.setFlipX(false);
+    } else {
+      this.player.setVelocityX(0);
+    }
 
-    //JUMP
+    const jumpPressed = Phaser.Input.Keyboard.JustDown(this.cursors.up) || Phaser.Input.Keyboard.JustDown(this.cursors.space) || Phaser.Input.Keyboard.JustDown(this.keys.jump);
 
-    // SHOOT
- 
-   // ENEMIES
- 
-
-    this.enemies.children.iterate((enemy) => {
-
-      if (enemy && enemy.update) {
-
-        enemy.update(this.time.now);
-      }
-    });
-
-
-    //TIMER
-   
-
-    const currentTime =
-      Math.floor(
-        (this.time.now - this.gameStartTime) / 1000
-      );
-
-    this.timerText.setText(
-      "TIEMPO: " + currentTime
-    );
+    if (jumpPressed && this.player.body.blocked.down) {
+      this.player.setVelocityY(-350);
+    }
   }
-
-  
- // SHOOT BULLET
- 
-
-  //HIT ENEMY
-  
-
-  hitEnemy(bullet, enemy) {
-
-    bullet.destroy();
-
-    enemy.takeDamage(1);
-  }
-
 }
